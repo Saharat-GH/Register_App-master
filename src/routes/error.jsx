fd459e4dd4063bf1bec3d3557a2e00057eea3f16
Saharat-GH <138373768+Saharat-GH@users.noreply.github.com>
2024-01-09 11:08:13 +0700

    create error page file

---
+++ src/routes/error.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../component/navBar";
+import Footer from "../component/footer";
+function Error() {
+    return (
+    <div className="Body">
+        <NavBar/>
+        error page
+        <Footer/>
+    </div>
+    )
+}
+export default Error;
