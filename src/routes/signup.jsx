b5e650acd5570274e21349bf9a54e3feea11acec
Saharat-GH <138373768+Saharat-GH@users.noreply.github.com>
2024-01-09 14:50:56 +0700

    remove HRCreateProjectComponent from SignUp.jsx

--- src/routes/signup.jsx
+++ src/routes/signup.jsx
@@ -1,13 +1,11 @@
 import NavBar from "../component/navBar";
 import ProjectBox from "../component/projectBox";
 import Footer from "../component/footer";
-import HRCreateProject from "./HRCreateProject";
 function SignUp() {
     return (
     <div className="Body">
         <NavBar/>
         <ProjectBox/>
-        <HRCreateProject />
         <Footer/>
     </div>
     )
