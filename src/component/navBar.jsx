302ac83ce3870c0013d29015d5cff9acc3cdb3da
Saharat-GH <138373768+Saharat-GH@users.noreply.github.com>
2024-01-10 11:15:31 +0700

    change Link to NavLink in NavBar.jsx

--- src/component/navBar.jsx
+++ src/component/navBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../assets/Logo.svg";
 import { useState } from "react";
 import LoginPopup from "./LoginPopup";
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
   const [login, setLogin] = useState(false);
@@ -20,25 +20,25 @@ export default function NavBar() {
         id="Nav"
       >
         <div className="col-md-3">
-          <Link
+          <NavLink
             to="/"
             className="d-inline-flex link-body-emphasis text-decoration-none"
           >
             <img src={Logo} alt="Your Logo" width="120" height="75" />
-          </Link>
+          </NavLink>
         </div>
 
         <div className="d-flex align-items-center ms-auto">
           <ul className="nav justify-content-center mb-md-0 me-3">
             <li>
-              <Link to="/" className="nav-link px-2 link-secondary text-warning">
+              <NavLink to="/" className="nav-link px-2 link-secondary text-warning">
                 Home
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="#" className="nav-link px-2 text-light">
+              <NavLink to="#" className="nav-link px-2 text-light">
                 About us
-              </Link>
+              </NavLink>
             </li>
           </ul>
 
